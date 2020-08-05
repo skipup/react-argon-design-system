262b8ce1601c08b241324888d688b86cfd749fcf
skipup <skipup123$>
2020-08-05 21:09:50 +0630

    👹

---
+++ src/Page/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from './Layout/Nav'
+const About = ()=>{
+    return(
+        <React.Fragment>
+            <Nav/>
+            <h1>About Page</h1>
+        </React.Fragment>
+    )
+}
+
+export  default About;
---
+++ src/Page/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from './Layout/Nav'
+const Home = ()=>{
+    return(
+        <React.Fragment>
+            <Nav/>
+            <h1>Home Page</h1>
+        </React.Fragment>
+    )
+}
+
+export  default Home;
